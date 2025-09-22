aecf613f2564409f9dc8e4d21adbb0ebe193ca96
Ishan2103 <ishanextra21@gmail.com>
2025-09-22 22:10:45 +0530

    Create swapping-using-pass-by-reference.cpp

---
+++ swapping-using-pass-by-reference.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+void swap(int &x, int &y) {
+    int temp = x;
+    x = y;
+    y = temp;
+}
+int main() {
+    int x = 34;
+    int y = 24;
+    cout << "Before swap:" << endl;
+    cout << "x = " << x << ", y = " << y << endl;
+    swap(x, y);  
+    cout << "After swap:" << endl;
+    cout << "x = " << x << ", y = " << y << endl;
+    return 0;
+}
